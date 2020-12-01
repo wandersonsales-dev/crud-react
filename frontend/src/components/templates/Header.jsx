9c0b956f932b9e78d009f4015ef3e51ab9c5ef11
Wanderson Sales <wandersonsales.dev@gmail.com>
2020-12-01 15:40:25 -0300

    Estrutura inicial

---
+++ frontend/src/components/templates/Header.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+import './Header.css'
+import React from 'react'
+
+export default props =>
+  <header className="header">
+    Cabeçalho
+  </header>
---
+++ frontend/src/components/templates/Logo.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+import './Logo.css'
+import React from 'react'
+
+export default props =>
+  <aside className="logo">
+    Logo
+  </aside>
