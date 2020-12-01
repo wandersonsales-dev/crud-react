b133204fea19d46ea7cf9299eeda72198cdf15dc
Wanderson Sales <wandersonsales.dev@gmail.com>
2020-12-01 18:13:22 -0300

    Componente Home

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -6,14 +6,13 @@ import React from 'react'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
-import Main from '../components/templates/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/templates/Footer'
 
 export default props =>
   <div className="app">
     <Logo />
     <Nav />
-    <Main icon="home" title="Início"
-      subtitle="Segundo Projeto do capítulo de React"/>
+    <Home />
     <Footer />
   </div>
