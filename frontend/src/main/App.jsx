80cd4a92fcd06b0c49407527f2ee98ae57f61577
Wanderson Sales <wandersonsales.dev@gmail.com>
2020-12-01 23:59:26 -0300

    Implementando Rotas

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -3,16 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
-import Home from '../components/home/Home'
+import Routes from './Routes'
 import Footer from '../components/templates/Footer'
 
 export default props =>
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Home />
-    <Footer />
-  </div>
+  <HashRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </HashRouter>
